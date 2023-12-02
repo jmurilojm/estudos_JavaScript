4e97fbdb8d33c53dc0fe21f0fafbaa902abcafda
José Murilo <82318481+jmurilojm@users.noreply.github.com>
2023-12-02 15:39:39 -0300

    Add files via upload

---
+++ cursos/cursoOneBitCode/OneBitCode/aula-02/ex001.js
@@ -0,0 +1,4 @@
+const frase = prompt('Digite uma frase')
+
+let tamanho = frase.length
+alert(`O tamanho da sua frase é de ${tamanho} caracteres.`)
---
+++ cursos/cursoOneBitCode/OneBitCode/aula-02/ex002.js
@@ -0,0 +1,14 @@
+let numero1 = prompt('Digite um número')
+let numero2 = prompt('Outro número')
+
+let soma = numero1 + numero2
+let subtracao = numero1 - numero2
+let multiplicacao = numero1 * numero2
+let divisao = numero1 / numero2
+
+alert(`Os resultados aritméticos com esses números são:
+
+Soma = ${soma}
+Subtração = ${subtracao}
+Multipicação = ${multiplicacao}
+Divisão = ${divisao}`)
