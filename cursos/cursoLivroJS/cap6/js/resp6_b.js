d996f3b4892935cf215f841ad45453c12bcbd5b3
j.murilo.87@gmail.com <j.murilo.87@gmail.com>
2023-12-19 13:36:16 -0300

    update resp6_b.js

--- cursos/cursoLivroJS/cap6/js/resp6_b.js
+++ cursos/cursoLivroJS/cap6/js/resp6_b.js
@@ -5,6 +5,8 @@ Nome do Autor: Mauricio Samy Silva
 Citação Bibliográfica: SILVA, M. S.
 */
 
+
+/*
 let nome = 'Jose Murilo Lopes Ferreira'
 nome = nome.trim().split(' ')
 let partes = []
@@ -26,5 +28,40 @@ for(let i = 0; i < (partes.length - 1); i++){
   citacao += partes[i] + '. '
 }
 console.log(citacao)
+*/
 
 
+const btGerar = document.getElementById('btGerar')
+btGerar.addEventListener('click', gerarCitacao)
+
+
+function gerarCitacao(){
+  const inNome = document.getElementById('inNome')
+  const outRetorno = document.getElementById('outRetorno')
+  
+  let nome = inNome.value
+  const retorno = outRetorno
+  nome = nome.trim().toUpperCase().split(' ')
+  let partes = []
+  
+  for (let i = 0; i < nome.length; i++) {
+    if (i == (nome.length - 1)) {
+      partes.push(nome[i])
+    } else {
+      partes.push(nome[i].charAt(0))
+    }
+  }
+  
+  
+  let citacao = ''
+  
+  citacao = partes[partes.length - 1] + ', '
+  
+  for (let i = 0; i < (partes.length - 1); i++) {
+    citacao += partes[i] + '. '
+  }
+  
+  
+  retorno.textContent = 'Citação Bibliográfica: ' + citacao
+  inNome.value = ''
+}
