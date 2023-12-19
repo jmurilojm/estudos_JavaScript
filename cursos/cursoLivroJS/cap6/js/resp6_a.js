e40de1058c28c407b9b63a3aa20cc1bccdbf8a4f
j.murilo.87@gmail.com <j.murilo.87@gmail.com>
2023-12-19 10:50:15 -0300

    update resp6_a.js

--- cursos/cursoLivroJS/cap6/js/resp6_a.js
+++ cursos/cursoLivroJS/cap6/js/resp6_a.js
@@ -2,6 +2,7 @@
 a) Você deve desenvolver um programa de criptografia para transmitir mensagens entre amigos. O programa deve ler uma mensagem e, em seguida, exibi-la criptografada. A criptografia consiste em: a) exibir todas as letras das posições pares da mensagem; b) exibir todas as letras das posições impares da mensagem. A Figura 6.15 exibe a mensagem criptografada. O programa deve conter ainda um botão para decriptografar a mensagem, ou seja, retornar a mensagem original a partir do texto cifrado (Figura 6.16).
 */
 
+/*
 const mensagem = 'jose murilo'
 
 let criptografado = ''
@@ -21,5 +22,38 @@ for(let i = 0; i < mensagem.length; i++){
 }
 
 console.log(criptografado + descriptografado)
+*/
 
 
+const btCriptografar = document.getElementById('btCriptografar')
+//const btDescriptografar = document.getElementById('btDescriptografar')
+
+btCriptografar.addEventListener('click', criptografarMensagem)
+//btDescriptografar.addEventListener('click', descriptografarMensagem)
+
+
+function criptografarMensagem(){
+  const inMensagem = document.getElementById('inMensagem')
+  const outRetorno = document.getElementById('outRetorno')
+  
+  const mensagem = inMensagem.value
+  const retorno = outRetorno
+  
+  let criptografado = ''
+  let descriptografado = ''
+
+  for(let i = 0; i < mensagem.length; i++){
+    //console.log(mensagem.charAt(i))
+    if(i % 2 == 0){
+    criptografado += mensagem.charAt(i)
+  }
+}
+for(let i = 0; i < mensagem.length; i++){
+  //console.log(mensagem.charAt(i))
+  if(i % 2 != 0){
+    criptografado += mensagem.charAt(i)
+  }
+}
+
+retorno.textContent = criptografado.toLowerCase()
+}
