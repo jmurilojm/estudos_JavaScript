af594a47252457403c8d6796d7f3ab0c611e0c1e
j.murilo.87@gmail.com <j.murilo.87@gmail.com>
2023-12-19 11:31:19 -0300

    criado o arquivo para resposta do 6_b

---
+++ cursos/cursoLivroJS/cap6/js/resp6_b.js
@@ -0,0 +1,4 @@
+/*
+b) Elaborar um programa que leia o nome de um autor de um livro. Em seguida, exiba-o no formato de uma citação bibliográfica com o sobrenome, seguido pelas iniciais do nome, conforme o exemplo ilustrado na Figura 6.17.
+*/
+
