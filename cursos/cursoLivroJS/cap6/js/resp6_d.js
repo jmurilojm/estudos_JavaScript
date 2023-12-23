a403b298e99f9e3c2ea939c65157f40e15f66228
j.murilo.87@gmail.com <j.murilo.87@gmail.com>
2023-12-23 10:05:12 -0300

    update resp6_d.js

--- cursos/cursoLivroJS/cap6/js/resp6_d.js
+++ cursos/cursoLivroJS/cap6/js/resp6_d.js
@@ -13,16 +13,27 @@ function calcularDataLimite(){
   const outRetorno = document.getElementById('outRetorno')
   
   
-  const data = inData.value
+  const dataDoc = inData.value
   const valor = Number(inValor.value)
   const retorno = outRetorno
   
   
-  let dataDoc = new Date()
-  dataDoc.setDate()
-  dataDoc.setMonth()
-  dataDoc.setFullYear()
+  const diaDoc = dataDoc.slice(8)
+  const mesDoc = dataDoc.slice(5,7)-1
+  const anoDoc = dataDoc.slice(0,4)
+  
+  const calendarioDoc = new Date(anoDoc,mesDoc,diaDoc)
   
   
-  console.log()
+  /*console.log(calendarioDoc.getDate(), (calendarioDoc.getMonth()+1), calendarioDoc.getFullYear())*/
+  
+  calendarioDoc.setDate(calendarioDoc.getDate() + 90)
+  
+  const dataLimite = calendarioDoc.getDate() + '/' + (calendarioDoc.getMonth()+1) + '/' + calendarioDoc.getFullYear()
+  
+  const TX = 0.2
+  const valorComDesconto = (valor - (valor * TX)).toFixed(2)
+  
+  retorno.textContent = 'Data Limite: ' + dataLimite + '\n\nAté essa data, você pagará R$: ' + valorComDesconto
+  
 }
