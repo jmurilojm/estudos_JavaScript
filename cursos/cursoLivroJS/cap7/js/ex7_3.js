8e7056cf520afa8aa5c6be00eb4694f2acde80e8
j.murilo.87@gmail.com <j.murilo.87@gmail.com>
2023-12-24 01:43:23 -0300

    update ex7_3.js

---
+++ cursos/cursoLivroJS/cap7/js/ex7_3.js
@@ -0,0 +1,4 @@
+/*
+Vamos agora construir um exemplo com uso de funções com retorno de valor. Nosso programa será para a Revenda Herbie (a mesma do Exemplo 5.3).Ele deve ler modelo, ano de fabricação e preço do veículo (o valor que o proprietário deseja receber pelo veículo). Em seguida, o programa deve classificar o veículo como: "Novo" (do ano atual), "Seminovo" (até 2 anos de uso) ou "Usado". Também deve apresentar o preço de venda do veículo com um acréscimo de 8% no preço dos veículos novos ou de 10% no preço dos veículos classificados como seminovos ou usados. Para a classificação e o cálculo do preço de venda do veículo, serão utilizadas funções com retorno de valor. A Figura 7.1 ilustra a página com um veículo para exemplificar os dados de entrada e saída do programa.
+*/
+
