ff71e311b3038566c2dcda7f993b2e36f3a6b432
j.murilo.87@gmail.com <j.murilo.87@gmail.com>
2023-12-26 10:04:30 -0300

    update resp7_c.js

---
+++ cursos/cursoLivroJS/cap7/js/resp7_c.js
@@ -0,0 +1,8 @@
+/*
+c) Elaborar um programa para uma veterinária, o qual leia o preço de uma vacina e se o cliente possui ou não convênio. Caso possua algum convênio, exibir uma caixa de seleção com os convênios "Amigo dos Animais" e "Saúde Animal. O programa deve exibir o valor do desconto (10% sem convênio; 20% para "Amigo dos Animais"; 50% para "Saúde Animal") e o valor a ser pago. Criar a função:
+
+• calcularDesconto() que receba os parâmetros valor e taxa de desconto. Retornar o valor do desconto.
+
+A Figura 7.7 apresenta a tela inicial do programa para um cliente sem convênio. Já a Figura 7.8 exibe a página para um cliente que possui convênio. Observe que a caixa de seleção não deve ser exibida no início do programa.
+*/
+
