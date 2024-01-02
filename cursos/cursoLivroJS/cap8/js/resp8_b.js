f48fc5cad8ae0a1338e4fb24fe0aa4d39d1e05c3
j.murilo.87@gmail.com <j.murilo.87@gmail.com>
2024-01-02 13:18:46 -0300

    update resp 8.b e ativação do link bootstrab em ex8_3

---
+++ cursos/cursoLivroJS/cap8/js/resp8_b.js
@@ -0,0 +1,50 @@
+/*
+b) Elaborar um programa para cadastrar produtos na lista de compras da semana. Salvar e exibir a listagem dos produtos em ordem alfabética.
+A Figura 8.8 exibe a ilustração da página com alguns dados inseridos.
+*/
+
+
+
+let listaDeCompras = []
+
+
+if(localStorage.getItem('lista')){
+  document.getElementById('outRetorno').textContent = localStorage.getItem('lista')
+}
+
+const btAdicionar = document.getElementById('btAdicionar')
+btAdicionar.addEventListener('click', function(){
+  
+  const inProduto = document.getElementById('inProduto')
+  const outRetorno = document.getElementById('outRetorno')
+  
+  
+  const produto = inProduto.value
+  const retorno = outRetorno
+  
+  
+  if(produto == ''){
+    alert('Por favor, insira um produto!')
+    return
+  }
+  
+  
+  listaDeCompras.push(produto.trim())
+  inProduto.value = ''
+  inProduto.focus()
+  localStorage.setItem('lista', listaDeCompras)
+  
+  retorno.textContent = localStorage.getItem('lista')
+})
+
+
+
+const btLimpar = document.getElementById('btLimpar')
+btLimpar.addEventListener('click', function(){
+  if(localStorage.getItem('lista')){
+    localStorage.removeItem('lista')
+    document.getElementById('outRetorno').textContent = ''
+  } else{
+    alert('Não existe lista para apagar.')
+  }
+})
