702d9010473b206add2a0c07f10c3e36ce86b61c
j.murilo.87@gmail.com <j.murilo.87@gmail.com>
2024-01-08 00:20:26 -0300

    update ex9_2.js

--- cursos/cursoLivroJS/cap9/js/ex9_2.js
+++ cursos/cursoLivroJS/cap9/js/ex9_2.js
@@ -39,3 +39,63 @@ function criarMoeda(num, pai, imgMoeda, textoAlt, classe){
   const br = document.createElement('br')
   pai.appendChild(br)
 }
+
+
+
+function conferirSoma(){
+  const inSoma = document.getElementById('inSoma')
+  
+  const soma = Number(inSoma.value)
+  
+  
+  if(soma == ''){
+    alert('Por favor, informa o valor!')
+    inSoma.focus()
+    return
+  }
+  
+  const divMoedas = document.getElementById('divMoedas')
+  const moedas = document.getElementsByTagName('img')
+  
+  
+  let totalMoedas = 0
+  for(let i = 0; i < moedas.length; i++){
+    if(moedas[i].className == 'moeda1_00'){
+      totalMoedas += 1.00
+    } else if(moedas[i].className == 'moeda0_50'){
+      totalMoedas += 0.50
+    } else if(moedas[i].className == 'moeda0_25'){
+      totalMoedas += 0.25
+    } else if (moedas[i].className == 'moeda0_10') {
+      totalMoedas += 0.10
+    }
+  }
+  
+  const div = document.createElement('div')
+  const h3 = document.createElement('h3')
+  
+  if(soma == totalMoedas.toFixed(2)){
+    div.className = 'alert alert-success'
+    var mensagem = 'Parabens!'
+  } else{
+    div.className = 'alert alert-danger'
+    var mensagem = 'Você errou! Valor R$: ' + totalMoedas.toFixed(2)
+  }
+  
+  const texto = document.createTextNode(mensagem)
+  h3.appendChild(texto)
+  
+  div.appendChild(h3)
+  divMoedas.appendChild(div)
+  
+  btConferir.disabled = 'true'
+}
+const btConferir = document.getElementById('btConferir')
+btConferir.addEventListener('click', conferirSoma)
+
+
+
+const btExibir = document.getElementById('btExibir')
+btExibir.addEventListener('click', function(){
+  location.reload()
+})
