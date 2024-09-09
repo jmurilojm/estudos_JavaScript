3d87523250a33b00eb06bf04620003bc49b0b8f6
José Murilo <82318481+jmurilojm@users.noreply.github.com>
2024-09-09 13:40:35 -0300

    Add files via upload

---
+++ cursoLivroJS/cap9/js/ex9_1.js
@@ -0,0 +1,142 @@
+/*
+Vamos construir um exemplo em que esses métodos são utilizados. Como de costume, inicie pela criação da pasta cap9 e, dentro dela, css, img e js. Nosso primeiro programa tem como objetivo adicionar itens em uma lista, processo semelhante ao já desenvolvido em outras páginas deste livro. Contudo, os itens da lista são agora inseridos a partir de tags HTML criadas pelo programa JavaScript. Também será possível selecionar linhas, recuperar seu conteúdo e remover uma tag no documento. A Figura 9.2 ilustra a página web do programa "Tarefas do Dia, já com algumas tarefas adicionadas.
+*/
+
+
+
+function adicionarTarefa(){
+  const inTarefa = document.getElementById('inTarefa')
+  
+  const tarefa = inTarefa.value
+  
+  if(tarefa == ''){
+    alert('Por favor, informe uma tarefa!')
+    inTarefa.focus()
+    return
+  }
+  
+  // agora, criar elemento html
+  // onde sera colocado a nova tag
+  const divQuadro = document.getElementById('divQuadro')
+  
+  // cria a tag e o texto para a tag
+  const h5 = document.createElement('h5')
+  const texto = document.createTextNode(tarefa)
+  
+  // indicar que texto é de h5 e h5 é de ...
+  h5.appendChild(texto)
+  divQuadro.appendChild(h5)
+  
+  inTarefa.value = ''
+  inTarefa.focus()
+}
+const btAdicionar = document.getElementById('btAdicionar')
+btAdicionar.addEventListener('click', adicionarTarefa)
+
+
+
+function selecionarTarefa(){
+  const h5 = document.getElementsByTagName('h5')
+  
+  const totalH5 = h5.length
+  
+  if(totalH5 == 0){
+    alert('Não há tarefas para selecionar!')
+    return
+  }
+  
+  let aux = -1
+  for(let i = 0; i < totalH5; i++){
+    if(h5[i].className == 'tarefaSelecionada'){
+      h5[i].className = 'tarefaNormal'
+      aux = i
+      break
+    }
+  }
+  
+  if(aux == totalH5 - 1){
+    aux = -1
+  }
+  
+  h5[aux + 1].className = 'tarefaSelecionada'
+}
+const btSelecionar = document.getElementById('btSelecionar')
+btSelecionar.addEventListener('click', selecionarTarefa)
+
+
+
+function retirarSelecionado(){
+  const divQuadro = document.getElementById('divQuadro')
+  const h5 = document.getElementsByTagName('h5')
+  
+  const totalH5 = h5.length
+  if(totalH5 == 0){
+    alert('Não há tarefa para ser retirada!')
+    return
+  }
+  
+  let aux = -1
+  for(let i = 0; i < totalH5; i++){
+    if(h5[i].className == 'tarefaSelecionada'){
+      aux = i
+      break
+    }
+  }
+  
+  if(aux == -1){
+    alert('Selecione uma tarefa para removela.')
+    return
+  }
+  
+  if(confirm('Confirma a exclusão de ' + h5[aux].textContent + '?')){
+    divQuadro.removeChild(h5[aux])
+  }
+}
+const btRetirar = document.getElementById('btRetirar')
+btRetirar.addEventListener('click', retirarSelecionado)
+
+
+
+function gravarTarefa(){
+  const h5 = document.getElementsByTagName('h5')
+  
+  const totalH5 = h5.length
+  if(totalH5 == 0){
+    alert('Não há o que salvar!')
+    return
+  }
+  
+  let tarefas = ''
+  for(let i = 0; i < totalH5; i++){
+    tarefas += h5[i].textContent + ';'
+  }
+  
+  localStorage.setItem('tarefasDoDia', tarefas.substr(0, tarefas.length -1))
+  
+  if(localStorage.getItem('tarefasDoDia')){
+    alert('As tarefas foram salvas!')
+    return
+  }
+  
+}
+const btGravar = document.getElementById('btGravar')
+btGravar.addEventListener('click', gravarTarefa)
+
+
+
+function recuperarTarefas(){
+  if(localStorage.getItem('tarefasDoDia')){
+    const tarefas = localStorage.getItem('tarefasDoDia').split(';')
+    
+    const divQuadro = document.getElementById('divQuadro')
+    
+    for(let i = 0; i < tarefas.length; i++){
+      const h5 = document.createElement('h5')
+      const texto = document.createTextNode(tarefas[i])
+      
+      h5.appendChild(texto)
+      divQuadro.appendChild(h5)
+    }
+  }
+}
+recuperarTarefas()
---
+++ cursoLivroJS/cap9/js/ex9_2.js
@@ -0,0 +1,107 @@
+/*
+Vamos construir um exemplo em que um programa JavaScript faz a inserção de imagens na página. Pensei em uma brincadeira que pode ser feita com crianças para que elas calculem a soma de moedas exibidas de forma aleatória na página. A cada execução, o programa deve gerar um novo número de moedas e a criança deve informar a soma e conferir se o cálculo que fez está correto. A Figura 9.5 exibe a página inicial do programa.
+*/
+
+
+
+function exibirMoedas(){
+  // gerar número aleatório
+  const num1_00 = Math.ceil(Math.random() * 5)
+  const num0_50 = Math.ceil(Math.random() * 5)
+  const num0_25 = Math.ceil(Math.random() * 5)
+  const num0_10 = Math.ceil(Math.random() * 5)
+  
+  // onde serão inseridas as moedas
+  const divMoedas = document.getElementById('divMoedas')
+  
+  // textos para a propriedade alt das imgs
+  const alt1_00 = 'Moeda de Um Real'
+  const alt0_50 = 'Moeda de Cinquenta centavos'
+  const alt0_25 = 'Moeda de Vinte e Cinco centavos'
+  const alt0_10 = 'Moeda de Dez centavos'
+  
+  // chamada de metodo
+  criarMoeda(num1_00, divMoedas, 'moeda1_00.png', alt1_00, 'moeda1_00')
+  criarMoeda(num0_50, divMoedas, 'moeda0_50.png', alt0_50, 'moeda0_50')
+  criarMoeda(num0_25, divMoedas, 'moeda0_25.png', alt0_25, 'moeda0_25')
+  criarMoeda(num0_10, divMoedas, 'moeda0_10.png', alt0_10, 'moeda0_10')
+}
+exibirMoedas()
+
+
+
+function criarMoeda(num, pai, imgMoeda, textoAlt, classe){
+  // inserir varias imagens na pagina
+  for(let i = 0; i < num; i++){
+    const novaMoeda = document.createElement('img')
+    
+    novaMoeda.src = 'img/' + imgMoeda
+    novaMoeda.textoAlt = textoAlt
+    novaMoeda.className = classe
+    
+    pai.appendChild(novaMoeda)
+  }
+  
+  const br = document.createElement('br')
+  pai.appendChild(br)
+}
+
+
+
+function conferirSoma(){
+  const inSoma = document.getElementById('inSoma')
+  
+  const soma = Number(inSoma.value)
+  
+  
+  if(soma == ''){
+    alert('Por favor, informa o valor!')
+    inSoma.focus()
+    return
+  }
+  
+  const divMoedas = document.getElementById('divMoedas')
+  const moedas = document.getElementsByTagName('img')
+  
+  
+  let totalMoedas = 0
+  for(let i = 0; i < moedas.length; i++){
+    if(moedas[i].className == 'moeda1_00'){
+      totalMoedas += 1.00
+    } else if(moedas[i].className == 'moeda0_50'){
+      totalMoedas += 0.50
+    } else if(moedas[i].className == 'moeda0_25'){
+      totalMoedas += 0.25
+    } else if (moedas[i].className == 'moeda0_10') {
+      totalMoedas += 0.10
+    }
+  }
+  
+  const div = document.createElement('div')
+  const h3 = document.createElement('h3')
+  
+  if(soma == totalMoedas.toFixed(2)){
+    div.className = 'alert alert-success'
+    var mensagem = 'Parabens!'
+  } else{
+    div.className = 'alert alert-danger'
+    var mensagem = 'Você errou! Valor R$: ' + totalMoedas.toFixed(2)
+  }
+  
+  const texto = document.createTextNode(mensagem)
+  h3.appendChild(texto)
+  
+  div.appendChild(h3)
+  divMoedas.appendChild(div)
+  
+  btConferir.disabled = 'true'
+}
+const btConferir = document.getElementById('btConferir')
+btConferir.addEventListener('click', conferirSoma)
+
+
+
+const btExibir = document.getElementById('btExibir')
+btExibir.addEventListener('click', function(){
+  location.reload()
+})
